71c0530727895894bb320d5783bc8b8a4fc6c71a
0x0is1 <0x0is1@protonmail.com>
2024-07-03 02:08:17 +0530

    [+] Fixed clipboard

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -26,7 +26,7 @@ function App() {
 
 
   const handleCopyClick = () => {
-    setContentToCopy(ltxcbRef.current.innerText)
+    setContentToCopy(ltxcbRef.current.innerText);
     navigator.clipboard.writeText(contentToCopy)
       .then(() => {
         console.log('Content copied successfully!');
@@ -36,23 +36,23 @@ function App() {
       });
   };
 
-  
+
   return (
     <div className="App bg-gray-100 m-2 p-4 sm:p-8">
       <h1 className='text-4xl mb-4 font-bold text-center md:text-left pl-8'>Canvas Drawing</h1>
       <div className='flex flex-col sm:flex-row gap-4'>
         <div className='w-full sm:w-3/5 shadow-lg bg-gray-200 rounded-lg p-4'>
-          <Canvas canvasRef={canvasRef}/>
+          <Canvas canvasRef={canvasRef} />
         </div>
         <div className='w-full sm:w-2/5 flex flex-col gap-4'>
           <div className='flex-1 bg-white p-4   shadow-md rounded-lg border'>
-              <h1 className='text-2xl font-bold mb-2'>
+            <h1 className='text-2xl font-bold mb-2'>
               ASCII Output
-              </h1>
-              <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
-                Content goes here
-              </div>
+            </h1>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
+              Content goes here
+            </div>
           </div>
           <div className='flex-1 bg-white p-4 shadow-md rounded-lg border'>
             <h1 className='text-2xl font-bold mb-2'>
@@ -62,9 +62,9 @@ function App() {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec
               Content goes here
             </div>
-            <button 
-            onClick={handleCopyClick}
-            className='mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg block mx-auto'>
+            <button
+              onClick={handleCopyClick}
+              className='mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg block mx-auto'>
               Copy
             </button>
           </div>
@@ -72,7 +72,7 @@ function App() {
       </div>
       <button
         onClick={getLatex}
-      className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg my-8 mx-auto block'>
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg my-8 mx-auto block'>
         Get LaTeX
       </button>
     </div>
