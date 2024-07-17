9c0f1cf59cc5f17684124e6025fd5ca58fcfb20c
0x0is1 <0x0is1@protonmail.com>
2024-07-17 17:11:15 +0530

    [+] Embedded backend and frontend in electron app

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ rar x data.rar
 - After installing all the requiremnets by
 
 ```
-pip install -r requiremnets.txt
+pip install -r requirements.txt
 ```
 
 make sure you are in the Backend directory.
@@ -36,4 +36,46 @@ make sure you are in the Backend directory.
 
 ```
 python trainmodel.py
+```
+
+## Electron build guide
+
+User can build their own electron application to run frontend and backend together
+
+### Install deps
+
+```sh
+npm install
+```
+
+### Run electron app
+
+Before running the electron app, you need to build the frontend react app. To do that, follow these:
+
+```sh
+cd frontend
+npm build
+```
+
+Now electron is ready to run:
+
+```sh
+cd .. # don't do that if you are not in frontend's parent directory
+npm start
+# or
+npx electron .
+```
+
+### Build binary for your platform
+
+As of now, build binary is facing some issue in starting, but might work on diff platforms
+```sh
+# for linux
+npx electron-builder --linux deb tar.xz
+
+# build for Windows ia32
+npx electron-builder --win --ia32
+
+# build for macOS, Windows and Linux
+npx electron-builder -mwl
 ```
---
+++ main.js
@@ -0,0 +1,58 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { exec } = require('child_process');
+let mainWindow;
+let pythonProcess;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            webSecurity: false,
+        },
+        show: false,
+    });
+    mainWindow.setMenuBarVisibility(false)
+    mainWindow.maximize()
+    mainWindow.loadFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    });
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    });
+}
+
+app.on('ready', () => {
+    pythonProcess = exec('cd backend && python app.py', (err, stdout, stderr) => {
+        if (err) {
+            console.error(`Error starting Python server: ${err}`);
+            return;
+        }
+        console.log(`Python server output: ${stdout}`);
+    });
+
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('quit', () => {
+    if (pythonProcess) {
+        pythonProcess.kill();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
